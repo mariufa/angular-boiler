16dc25d03b812bd64e668eec5ec08a20e5a08e7b
Marius Fagerland <mariusfagerland@yahoo.no>
2017-09-14 17:46:00 +0200

    Add component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BasicDetailComponent } from './basic-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BasicDetailComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/basic-detail.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'basic-detail',
+  templateUrl: './basic-detail.component.html'
+})
+
+export class BasicDetailComponent {
+  title = 'Basics'
+}
