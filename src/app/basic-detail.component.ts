bec56cf393da122747ddc9a8e5fdf07087062176
Marius Fagerland <mariusfagerland@yahoo.no>
2017-09-14 18:21:26 +0200

    Add BASICS

--- src/app/basic-detail.component.ts
+++ src/app/basic-detail.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
 
+import { Basic } from './basic';
+
+const BASICS: Basic[] = [
+  { name: 'Marius' },
+  { name: 'Test' }
+];
+
 @Component({
   selector: 'basic-detail',
   templateUrl: './basic-detail.component.html'
 })
 
 export class BasicDetailComponent {
-  title = 'Basics'
+  title = 'Basics';
+  basics = BASICS;
 }
